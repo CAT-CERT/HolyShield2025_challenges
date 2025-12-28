ee56406d4da0d9ab56730bc578851c14bb7758c1
aestera <aest3ra@gmail.com>
2025-12-28 11:14:30 +0900

    fix: delete server code

--- Rev/Time Keepers/server/src/minigames/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod first_minigame;
-pub mod second_minigame;
-pub mod third_minigame;
-pub mod fourth_minigame;
-pub mod fifth_minigame;
-pub mod check_keys;
-pub mod uis;
--- Rev/Time Keepers/server/src/minigames/uis/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod first_ui;
-pub mod second_ui;
-pub mod third_ui;
-pub mod fourth_ui;
-pub mod fifth_ui;
